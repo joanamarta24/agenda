5270a36952e9f495bc38eccd4d3f8a67913a95ba
jonamarta <joanamartasouza@gmail.com>
2026-03-20 21:48:11 -0300

    20/03

--- src/main/java/org/example/ContatoDAO.java
+++ src/main/java/org/example/ContatoDAO.java
@@ -1,4 +1,62 @@
 package org.example;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContatoDAO {
+    public void adicionar(Contato contato){
+        String sql ="INSERT INTO contatos(nome,telefone) VALUES(?,?)";
+        try (Connection conn = Database.conectar();
+             PreparedStatement pstmt = conn.prepareStatement(sql){
+                 pstmt.setString(1, contato.getNome());
+                 pstmt.setString(1, contato.getTelefone());
+
+                 pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao adicionar contato: " + e.getMessage());
+        }
+        return Contato;
+    }
+    public List<Contato> listar(){
+        List<Contato> contatos = new ArrayList<>();
+        String sql = "SELECT * FROM contatos";
+        try (Connection conn = Database.connectar()
+        Statement stmt = conn.createStatement()
+        ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String telefone = rs.getString("telefone");
+
+                contatos.add(new Contato(id,nome, telefone));
+            }
+        }catch(SQLException e){
+            System.out.println("Erro ao listar contato: " + e.getMessage());
+        }
+        return contatos;
+    }
+    public  void  atualizar (Contato contato){
+        String sql = "UPDATE  contstos SET nome = ?, telefone = ? WHERE id =?";
+        try(Connection conn = Database.conectar();
+          PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1,contato.getNome());
+            pstmt.setString(2,contato.getTelefone());
+            pstmt.setInt(3,contato.getId());
+
+            pstmt.executeUpdate();
+        }catch (SQLException e){
+            System.out.println("Erro ao atualizar contato: " + e.getMessage());
+        }
+    }
+
+    public void  remover(int id){
+        String sql = "DELETE FROM contatos WHERE id = ?";
+        try (Connection conn = Database.conectar();
+             PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+        }
+        )
+    }
 }
